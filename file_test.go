bd4ef7967ed984d5344f13bdf8d2e584d475db33
Jade Auer <jda@tapodi.net>
2018-05-07 01:12:06 -0700

    adapt file load test to handle multiple test files and include non-gzipped test file. closes #2

--- file_test.go
+++ file_test.go
@@ -6,13 +6,22 @@ import (
 	"testing"
 )
 
+var testFiles = []string{
+	"S46W066.hgt.gz",
+	"S46W067.hgt",
+}
+
 func TestReadFile(t *testing.T) {
 	wd, _ := os.Getwd()
-	tFileName := path.Join(wd, "testdata", "S49W066.hgt.gz")
+	for _, tf := range testFiles {
+		t.Run(tf, func(t *testing.T) {
+			tFileName := path.Join(wd, "testdata", tf)
 
-	_, err := ReadFile(tFileName)
-	if err != nil {
-		t.Errorf("failed to process %s: %s", tFileName, err)
+			_, err := ReadFile(tFileName)
+			if err != nil {
+				t.Errorf("failed to process %s: %s", tFileName, err)
+			}
+		})
 	}
 
 }
